4ffa77b721b23d6faa6772c97e804915cacff3e4
jmlc643 <josemarialuyocampos@gmail.com>
2025-01-13 22:21:30 -0500

    First Commit

---
+++ src/main/java/com/recpro/pe/learnsync/LearnsyncApplication.java
@@ -0,0 +1,13 @@
+package com.recpro.pe.learnsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recpro/pe/learnsync/LearnsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recpro.pe.learnsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
