18e417446046cfe109f9a518600050f1cf311c70
jmlc643 <josemarialuyocampos@gmail.com>
2025-01-25 18:17:59 -0500

    Delete ThreadMapper and RoleMapper and Update ThreadService with Class Method

--- src/main/java/com/recpro/pe/learnsync/mappers/UserMapper.java
+++ src/main/java/com/recpro/pe/learnsync/mappers/UserMapper.java
@@ -2,16 +2,15 @@ package com.recpro.pe.learnsync.mappers;
 
 import com.recpro.pe.learnsync.dtos.auth.role.RoleDTO;
 import com.recpro.pe.learnsync.dtos.auth.user.UserDTO;
+import com.recpro.pe.learnsync.models.Role;
 import com.recpro.pe.learnsync.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
-    @Autowired private RoleMapper roleMapper;
 
     public UserDTO toDto(User user) {
-        RoleDTO role = roleMapper.toDto(user.getRole());
+        RoleDTO role = Role.toDto(user.getRole());
         return new UserDTO(user.getUsername(), user.getEmail(), user.getCreationDate(), user.getPoints(), role);
     }
 }
--- src/main/java/com/recpro/pe/learnsync/services/forum/ThreadService.java
+++ src/main/java/com/recpro/pe/learnsync/services/forum/ThreadService.java
@@ -3,7 +3,6 @@ package com.recpro.pe.learnsync.services.forum;
 import com.recpro.pe.learnsync.dtos.forum.thread.CreateThreadDTO;
 import com.recpro.pe.learnsync.dtos.forum.thread.ThreadDTO;
 import com.recpro.pe.learnsync.exceptions.ResourceNotExistsException;
-import com.recpro.pe.learnsync.mappers.ThreadMapper;
 import com.recpro.pe.learnsync.models.Thread;
 import com.recpro.pe.learnsync.models.Topic;
 import com.recpro.pe.learnsync.models.User;
@@ -19,19 +18,19 @@ import java.util.List;
 @Service
 public class ThreadService {
     @Autowired private ThreadRepository threadRepository;
-    @Autowired private ThreadMapper threadMapper;
     @Autowired private UserService userService;
     @Autowired private TopicService topicService;
 
     public List<ThreadDTO> listThreads(Pageable pageable) {
-        return threadRepository.findAll(pageable).stream().map(it -> threadMapper.toDTO(it)).toList();
+        return threadRepository.findAll(pageable).stream().map(Thread::toDTO).toList();
     }
 
     public ThreadDTO createThread(CreateThreadDTO request) {
         User user = userService.findByUser(request.getUsername());
         Topic topic = topicService.getTopic(request.getTopicname());
         Thread thread = new Thread(null, request.getTitle(), request.getMessage(), topic, user, new ArrayList<>());
-        return threadMapper.toDTO(threadRepository.save(thread));
+        threadRepository.save(thread);
+        return Thread.toDTO(thread);
     }
 
     public Thread getThread(Integer id) {
